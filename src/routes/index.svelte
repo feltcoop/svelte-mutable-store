de7cfb69be1a2378fe5d0ee0a5301ad631e24299
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2022-01-21 22:21:33 -0600

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Message from '@feltcoop/felt/ui/Message.svelte';
+
+	import Mreows from '$lib/Mreows.svelte';
+</script>
+
+<main class="markup column">
+	<section class="column-sm">
+		<h1>felt-template</h1>
+		<Message>hello and welcome</Message>
+	</section>
+	<Mreows />
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin: 0 auto;
+	}
+	h1 {
+		text-align: center;
+	}
+</style>
